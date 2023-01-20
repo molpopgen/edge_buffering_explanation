a08c293eabf06043f2d9530e8efb5dbd054de3ac
Kevin R. Thornton <krthornt@uci.edu>
2023-01-20 08:43:09 -0800

    consitent figure plotting method

--- plot.py
+++ plot.py
@@ -19,9 +19,8 @@ for g, d in benchmark.groupby(['N', 'simplify']):
 benchmark["time_rel"] = time_rel
 benchmark["mem_rel"] = mem_rel
 
-g = sns.FacetGrid(benchmark, col="simplify", hue="method")
-g.map(sns.scatterplot, "N", "time_rel")
-g.add_legend()
+g = sns.relplot(benchmark, x="N", y="time_rel",
+                col="simplify", hue="method", style="method")
 g.set(ylim=(0, 1.5))
 
 for ax in g.axes[0]:
@@ -30,29 +29,24 @@ for ax in g.axes[0]:
 
 plt.savefig("benchmark_time_rel.png")
 
-g = sns.FacetGrid(benchmark, col="simplify", hue="method")
-g.map(sns.scatterplot, "N", "mem_rel")
-g.add_legend()
-
+g = sns.relplot(benchmark, x="N", y="mem_rel",
+                col="simplify", hue="method", style="method")
 plt.savefig("benchmark_mem_rel.png")
 
 # Now, plot absolute timings/mem use
 
-g = sns.FacetGrid(benchmark, col="simplify", hue="method")
 time = benchmark["time"]/60.0/60.0
 benchmark["hours"] = time
-g.map(sns.scatterplot, "N", "hours", alpha=0.5)
+g = sns.relplot(benchmark, x="N", y="hours",
+                col="simplify", hue="method", style="method")
 g.set(ylabel="Time (hours)")
-g.add_legend()
 plt.savefig("benchmark_time.png")
 
 mem = benchmark["mem"]/1024.0/1024.0
 benchmark["GB"] = mem
 g = sns.relplot(benchmark, x="N", y="GB",
                 col="simplify", hue="method", style="method")
-# g.map(sns.scatterplot, "N", "GB", alpha=0.5)
 g.set(ylabel="Peak memory (GB)")
-g.add_legend()
 plt.savefig("benchmark_mem.png")
 
 # Time difference from bookmark
@@ -67,7 +61,6 @@ benchmark["hours_delta"] = th
 
 g = sns.relplot(benchmark, x="N", y="hours_delta",
                 col="simplify", hue="method", style="method")
-# g.map(sns.scatterplot, "N", "GB", alpha=0.5)
 g.set(ylabel="Time delta from bookmark (hours)")
 plt.savefig("benchmark_time_delta.png")
 
